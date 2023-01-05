3f722f7198848acfcfc01359532e3323a11a7bc8
苏小妍 <13666683140@163.com>
2023-01-05 17:50:46 +0800

    :sparkles: add 依赖

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
+/*
+ * @Descripttion:
+ * @Author: 苏小妍
+ * @LastEditors: 苏小妍
+ * @Date: 2023-01-05 16:38:23
+ * @LastEditTime: 2023-01-05 16:46:31
+ */
+
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        alias: {
+          "@": "./src",
+        },
+      },
+    ],
+    "react-native-reanimated/plugin",
+  ],
 };
